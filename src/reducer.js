caacf951c1c6510c56d045934dc4d9ee009029d2
Donte Ladatto <donte279@gmail.com>
2022-10-22 16:20:09 -0500

    commit tree e11813b37eff9c314276ea48780c38a596b0d7a8

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,28 @@
-// add code snippets from README
+let state;
+
+const changeState = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "counter/increment":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+
+const render = () => {
+  const container = document.getElementById("container");
+  container.textContent = state.count;
+}
+
+const dispatch = (action) => {
+  state = changeState(state, action)
+  render()
+}
+
+const button = document.getElementById("button");
+
+button.addEventListener("click", () => {
+  dispatch({ type: "counter/increment" });
+});
+
+dispatch({ type: "@@INIT" })
